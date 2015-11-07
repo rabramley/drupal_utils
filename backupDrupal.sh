6b4c0872d6ef1756ec3dfa1470e1f1cd09f029ce
Richard Bramley <rabramley@gmail.com>
2015-11-07 10:56:01 +0000

    Add backup and get new script

---
+++ backupDrupal.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+#
+# fullsitebackup.sh V1.0
+#
+# Full backup of website files and database content.
+#
+# A number of variables defining file location and database connection
+# information must be set before this script will run.
+# Files are tar'ed from the root directory of the website. All files are
+# saved. The MySQL database tables are dumped without a database name and
+# and with the option to drop and recreate the tables.
+#
+# Parameters:
+#    tar_file_name (optional)
+#
+# Configuration
+#
+
+. parameters.sh
+
+# Website Files
+  webrootdir=$WEB_ROOT$SITE_DIRECTORY  # (e.g.: webrootdir=/home/user/public_html)
+
+
+#
+# Variables
+#
+
+
+# Default TAR Output File Base Name
+  tarnamebase=sitebackup-
+  datestamp=`date +'%Y-%m-%d'`
+
+
+# Execution directory (script start point)
+  startdir=`pwd`
+
+
+# Temporary Directory
+  tempdir=tmpbckdir$datestamp
+
+
+#
+# Input Parameter Check
+#
+
+
+if test "$1" = ""
+  then
+    tarname=$tarnamebase$datestamp.tgz
+  else
+    tarname=$1
+fi
+
+
+#
+# Banner
+#
+echo ""
+echo "fullsitebackup.sh V1.0"
+
+
+#
+# Create temporary working directory
+#
+echo " .. Setup"
+mkdir $tempdir
+echo "    done"
+
+
+#
+# TAR website files
+#
+echo " .. TARing website files in $webrootdir"
+cd $webrootdir
+tar cf $startdir/$tempdir/filecontent.tar .
+echo "    done"
+
+
+#
+# sqldump database information
+#
+echo " .. sqldump'ing database:"
+echo "    user: $dbuser; database: $dbname; host: $dbhost"
+cd $startdir/$tempdir
+mysqldump -p --user=$dbuser --host=$dbhost --add-drop-table $dbname > dbcontent.sql
+echo "    done"
+
+
+#
+# Create final backup file
+#
+echo " .. Creating final compressed (tgz) TAR file: $tarname"
+tar czf $startdir/$tarname filecontent.tar dbcontent.sql
+echo "    done"
+
+
+#
+# Cleanup
+#
+echo " .. Clean-up"
+cd $startdir
+rm -r $tempdir
+echo "    done"
+
+
+#
+# Exit banner
+#
+echo " .. Full site backup complete"
+echo ""
+
---
+++ getNewDrupal.sh
@@ -0,0 +1,81 @@
+#!/bin/sh
+
+. parameters.sh
+
+echo -n "Enter the new Drupal version number (eg, X.XX): "
+read version
+
+echo -n "New version is '$version' do you want to continue? [y/N]: "
+read response
+
+if [ "$response" != "y" ]; then
+  echo "bye"
+  exit 1
+fi
+
+cd $WEB_ROOT
+
+echo "Getting new Drupal"
+wget http://ftp.drupal.org/files/projects/drupal-$version.zip
+
+echo "Unzipping new Drupal"
+unzip drupal-$version.zip
+
+echo "Copying customisations to new Drupal"
+rm -fR drupal-$version/sites
+cp -r $SITE_DIRECTORY/sites drupal-$version
+
+echo "Diffing HTACCESS"
+diff $SITE_DIRECTORY/.htaccess drupal-$version/.htaccess
+
+echo -n "That was the diff in the .htaccess ('<' in existing; '>' in new). Do you want to continue? [y/N]: "
+read response
+
+if [ "$response" != "y" ]; then
+  echo "bye"
+  exit 1
+fi
+
+echo "Diffing Web.Config"
+diff $SITE_DIRECTORY/web.config drupal-$version/web.config
+
+echo -n "That was the diff in the web.config ('<' in existing; '>' in new). Do you want to continue? [y/N]: "
+read response
+
+if [ "$response" != "y" ]; then
+  echo "bye"
+  exit 1
+fi
+
+echo "Diffing Robots.txt"
+diff $SITE_DIRECTORY/robots.txt drupal-$version/robots.txt
+
+echo -n "That was the diff in the robots.txt ('<' in existing; '>' in new). Do you want to continue? [y/N]: "
+read response
+
+if [ "$response" != "y" ]; then
+  echo "bye"
+  exit 1
+fi
+
+echo "Diffing Default Settings"
+diff $SITE_DIRECTORY/sites/default/default.settings.php drupal-$version/sites/default/default.settings.php
+
+echo -n "That was the diff in the default.settings.php ('<' in existing; '>' in new). Do you want to continue? [y/N]: "
+read response
+
+if [ "$response" != "y" ]; then
+  echo "bye"
+  exit 1
+fi
+
+echo "Archiving Existing site..."
+mv $SITE_DIRECTORY "$SITE_DIRECTORY"_pre_$version
+
+echo "Moving new site into place..."
+mv drupal-$version $SITE_DIRECTORY
+
+sudo /etc/init.d/uol.apache2 restart
+
+echo "Go to update.php and follow the instructions"
+echo "Bye for now"
