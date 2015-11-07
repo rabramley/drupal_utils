501db39649fbc75715c6da753b13fbb1cf736ce1
Richard Bramley <rabramley@gmail.com>
2015-11-07 10:58:16 +0000

    Add example parameters

---
+++ example_parameters.sh
@@ -0,0 +1,9 @@
+WEB_ROOT="/path/to/web/root"
+SITE_DIRECTORY="actual_drupal_directory"
+
+# Database connection information
+dbname=db_name     # (e.g.: dbname=drupaldb)
+dbhost=db_host
+dbuser=db_user # (e.g.: dbuser=drupaluser)
+dbpassword=db_password
+
